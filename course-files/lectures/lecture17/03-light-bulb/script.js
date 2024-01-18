e7cc74a2294a4519477bd912331e5b1a126fde0d
Sarah <vanwars@gmail.com>
2024-01-18 16:18:30 -0500

    Organizing the scripts

--- course-files/lectures/lecture17/03-light-bulb/script.js
+++
@@ -1,7 +0,0 @@
-const imgOn = "https://www.w3schools.com/js/pic_bulbon.gif";
-const imgOff = "https://www.w3schools.com/js/pic_bulboff.gif";
-const elem = document.querySelector("#my_image");
-
-function turnOn() {
-
-}
--- course-files/lectures/lecture18_19/00-while-loop-intro/main.js
+++
@@ -1,6 +0,0 @@
-console.log('Hey there! Hope you\'re doing OK!');
-
-// note: this is an infinite loop. This code will crash your browser:
-// while (true) {
-//     console.log('Hey there! Hope you\'re doing OK!');
-// }
